8fa0eb4d2a23fbb046c3379583139617290ad196
kimjava911 <aibackend4th@gmail.com>
2025-12-04 14:23:49 +0900

    step02

--- src/main/java/kr/java/sb/controller/MemoController.java
+++ src/main/java/kr/java/sb/controller/MemoController.java
@@ -1,11 +1,14 @@
 package kr.java.sb.controller;
 
+import kr.java.sb.entity.Memo;
 import kr.java.sb.repository.MemoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // ComponentScan + Web MVC
 //@RequestMapping // DispatcherServlet -> '/'
@@ -15,8 +18,20 @@ public class MemoController {
     // -> final을 포함한 생성자 -> @RequiredArgsConstructor
 
     @GetMapping // GetMapping -> "/" -> fetch, form : GET. ㅂ라우저 : 접속.
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("memos", memoRepository.findAll());
+        // Entity -> Data -> @ToString -> <%= memos %>
         // 뷰 경로
         return "index";
+    }
+
+    @PostMapping
+    // @RequestParam("content")
+    // -> configurations { compileOnly { extendsFrom annotationProcessor } }
+    public String form(@RequestParam String content) {
+        Memo memo = new Memo();
+        memo.setContent(content);
+        memoRepository.save(memo);
+        return "redirect:/"; // PRG
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -5,5 +5,12 @@
 </head>
 <body>
     <p>메모 조회 및 작성</p>
+
+    <%= request.getAttribute("memos") %>
+
+    <form method="post">
+        <input name="content" placeholder="메모 내용"><br>
+        <button>작성</button>
+    </form>
 </body>
 </html>
